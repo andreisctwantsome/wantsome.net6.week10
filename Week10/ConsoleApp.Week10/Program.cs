1b03ccefd1533cdb56f971fde2c923ce84bc5389
DanielaSimiganoschi <daniela.simiganoschi95@gmail.com>
2020-09-21 18:36:32 +0300

    change 1

--- Week10/ConsoleApp.Week10/Program.cs
+++ Week10/ConsoleApp.Week10/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp.Week10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World10!");
         }
     }
 }
